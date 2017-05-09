980febb240e58349fb2bfbebd7f1866871012fd4
alexkodak <neret.jeremy@gmail.com>
2017-05-09 14:18:09 +0100

    corrected models/input.js

---
+++ models/input.js
@@ -0,0 +1,9 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var InputSchema = new Schema({
+  user_id: {type: String},
+  tour: {type: String}
+});
+
+module.exports = mongoose.model("Facebook", InputSchema);
