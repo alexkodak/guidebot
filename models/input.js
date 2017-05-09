0201b81c06a275ec225aaf7dd53415bd995a10a5
alexkodak <neret.jeremy@gmail.com>
2017-05-09 14:37:54 +0100

    Add files via upload

--- models/input.js
+++ models/input.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 var db = mongoose.connect(process.env.MONGODB_URI);
 
+var Input = mongoose.model ('Input', InputSchema);
+
 var InputSchema = new mongoose.Schema({
   user_id: {type: String},
   tour: {type: String}
 });
-
-var Input = mongoose.model ('Input', schema);
 
 module.exports = mongoose.model('Input', InputSchema);
