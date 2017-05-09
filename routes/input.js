1d2cb718fff25a8bcf95d7ea03cf7802f94e7424
alexkodak <neret.jeremy@gmail.com>
2017-05-09 14:17:36 +0100

    Delete input.js

--- routes/input.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-
-var InputSchema = new Schema({
-  user_id: {type: String},
-  tour: {type: String}
-});
-
-module.exports = mongoose.model("Facebook", InputSchema);
