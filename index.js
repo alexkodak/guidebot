b7d26118e40055ee3307b277d94c83b6fb9d3db0
alexkodak <neret.jeremy@gmail.com>
2017-05-25 14:19:33 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ function findTour(userId, formattedMsg) {
     request("https://blooming-wave-81088.herokuapp.com/tours/" + formattedMsg, function (error, response, body, res) {
         if (!error && response.statusCode == 200) {
            
-            console.log("connection ok" + JSON.parse(body));
+            console.log("connection ok" + body);
           
             var inputObj = JSON.parse(body);
          
