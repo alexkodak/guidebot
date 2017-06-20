e7939cdbe7473f8c6f897833603721f914dbede4
alexkodak <neret.jeremy@gmail.com>
2017-06-20 17:17:39 +0100

    Add files via upload

--- models/input.js
+++ models/input.js
@@ -2,9 +2,12 @@ var mongoose = require("mongoose");
 
 var InputSchema = mongoose.Schema({
 	user_id: String,
-	tour: String,
+	tour: Number,
 	language: String,
-	Description: String
+	tour_description: String,
+	caption: String,
+	room: Number,
+	caption_description: String
 });
 
 module.exports = mongoose.model("Input", InputSchema);
