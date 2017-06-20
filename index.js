52706721168c06091c1f65d11abd4167aa100ade
alexkodak <neret.jeremy@gmail.com>
2017-06-20 12:57:20 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ function processMessage(event) {
             } else {
                 console.log("existing tour found1: " + JSON.stringify(body));
                 var userObj = body;
-                console.log("existing tour found2: " + userObj.tour);
+                console.log("existing tour found2: " + userObj);
                 findCaption(senderId, formattedMsg, userObj);
               }
     });
